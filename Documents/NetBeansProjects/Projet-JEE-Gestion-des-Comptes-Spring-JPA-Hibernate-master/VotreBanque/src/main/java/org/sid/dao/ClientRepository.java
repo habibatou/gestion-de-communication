ffc068b5e62733889a9bce300414723d2cd434b3
habibatou <habipenda93@gmail.com>
2018-11-07 16:08:23 +0000

    6eme commit

--- Documents/NetBeansProjects/Projet-JEE-Gestion-des-Comptes-Spring-JPA-Hibernate-master/VotreBanque/src/main/java/org/sid/dao/ClientRepository.java
+++ Documents/NetBeansProjects/Projet-JEE-Gestion-des-Comptes-Spring-JPA-Hibernate-master/VotreBanque/src/main/java/org/sid/dao/ClientRepository.java
@@ -2,6 +2,9 @@ package org.sid.dao;
 
 import org.sid.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+/*
+*Habi
+*/
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
